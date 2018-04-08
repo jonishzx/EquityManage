ff276fce739e0580c193bbde088f0f115906a574
jonishwzx <475896965@qq.com>
2018-04-08 09:13:02 +0800

    wzx

---
+++ UkeyTech.OA.WebApp/Areas/Equity/EquityAreaRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace UkeyTech.OA.WebApp.Areas.Equity
+{
+    public class EquityAreaRegistration : AreaRegistration
+    {
+        public override string AreaName {
+            get { return "Equity"; }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Equity_default",
+                "Equity/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
