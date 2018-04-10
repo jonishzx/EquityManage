279ecef66d1e1872d6cfbc6cc5cfa1768582fc72
jonishwzx <475896965@qq.com>
2018-04-10 10:26:05 +0800

    wzx

--- UkeyTech.OA.WebApp/ModuleJs/Permission/RolePermission.js
+++ UkeyTech.OA.WebApp/ModuleJs/Permission/RolePermission.js
@@ -96,7 +96,6 @@ function DeleteRoles() {
                                 hiddenOwnerValue = null;
                             }
                         });
-
                     }
                 });
             }
